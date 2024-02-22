9040de8496b08dd544b468d3a0f0f4489d7c3ea3
Sieghart <sieghart2005@gmail.com>
2024-02-22 12:37:48 -0500

    hero de la pagina principal creado

---
+++ script.js
@@ -0,0 +1,23 @@
+let tween = gsap.from(".text",{
+    duration:0.5,
+    y:200,
+    scale:0,
+    stagger:0.25
+})
+
+gsap.from(".hero",{
+    backgroundColor:"#615B6B",
+    duration:2
+})
+
+gsap.to(".hero",{
+    backgroundColor:"#8267AB",
+    duration:2,
+    ease:"linear"
+})
+
+gsap.to("#img",{
+    boxShadow:"1rem 1rem .5rem rgba(0,0,0,0.3)",
+    duration:5,
+    
+})

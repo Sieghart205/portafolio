914298bc38a84b8189c2b5fbc86960151667c2f0
Sieghart <sieghart2005@gmail.com>
2024-02-24 19:44:29 -0500

    redes sociales del index  en funcionamiento, finalización de la vista del portafolio

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 let tween = gsap.from(".text",{
     duration:0.5,
     y:200,
-    scale:0,
+    x:-5,
+    opacity:0,
     stagger:0.25,
     delay:0.5,
+    ease:"in",
+})
+
+gsap.from("#nombre",{
+    scale:0,
+    y:-400,
+    duration:0.5,
+})
+
+gsap.from("#img",{
+    scale:0,
+    duration:0.5,
 })
 
 gsap.from(".hero",{
     backgroundColor:"#615B6B",
-    duration:2
+    duration:2,
+    delay:0.5,
 })
 
 gsap.to(".hero",{
     backgroundColor:"#8267AB",
     duration:2,
-    ease:"linear"
+    ease:"linear",
+    delay:0.5,
 })
 
 gsap.to("#img",{
     boxShadow:"1rem 1rem .5rem rgba(0,0,0,0.3)",
     duration:5,
-    
+    delay:0.5,
 })
 
-const ws = document.getElementById("whatsapp");
+const f = document.getElementById("facebook");
+const x = document.getElementById("x");
+const p = document.getElementById("pinterest");
 
-ws.addEventListener("click",()=>{
-    window.open("https://wa.me/573226508681")
+f.addEventListener("click",()=>{
+    window.open("https://www.facebook.com/profile.php?id=100075133152053");
+})
+
+x.addEventListener("click",()=>{
+    window.open("https://twitter.com/JeissonBenitez3")
+})
+
+p.addEventListener("click",()=>{
+    window.open("https://co.pinterest.com/benitezortegajeisson/_created/")
+})
+
+window.addEventListener("scroll",()=>{
+    let header = document.getElementById("header");
+    header.classList.toggle("abajo",window.scrollY>700);
 })
